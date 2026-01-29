70664784dbb5fb1b5f6e3780291feb69f4dbe4fd
Gitops+Project <laxmanraju.ganapathi123@gmail.com>
2026-01-29 11:17:44 +0530

    latest updates

--- terraform/eks-cluster.tf
+++ terraform/eks-cluster.tf
@@ -18,7 +18,7 @@
 #     one = {
 #       name = "node-group-1"
 
-#       instance_types = ["t3.small"]
+#       instance_types = ["t3.small"] ##
 
 #       min_size     = 1
 #       max_size     = 3
